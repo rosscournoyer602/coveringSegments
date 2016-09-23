b10eac5b111efa0b29451f5a863439efce10c0e6
Ross Cournoyer <rcoupvc@yahoo.com>
2016-09-23 16:08:28 +0800

    Update CoveringSegments.java

--- CoveringSegments.java
+++ CoveringSegments.java
@@ -1,34 +1,35 @@
-iimport java.util.*;
+package signatures;
+
+import java.util.*;
 
 public class CoveringSegments {
 
     private static int[] optimalPoints(Segment[] segments) {
         
         ArrayList<Integer> pointList = new ArrayList<>();
-        ArrayList<Segment> segmentList = new ArrayList<>();
+        ArrayList<Segment> segList = new ArrayList<>();
         int minEnd;
         
-        segmentList.addAll(Arrays.asList(segments));
+        segList.addAll(Arrays.asList(segments));
         
-        while (segmentList.size() > 1) {  
-            minEnd = calcMinEnd(segmentList);
-            pointList.add(minEnd);
-            segmentList = segmentRemover(segmentList,minEnd);
+        while (segList.size() > 0) {            //main greedy loop
+            minEnd = calcMinEnd(segList);       //find minEnd
+            pointList.add(minEnd);              //add to point list
+            segList = segRemove(segList,minEnd);//remove segs covered by minEnd
         }
         
         int[] points = new int[pointList.size()];
         
-        for (int i=0; i < pointList.size(); ++i) {
-            points[i] = pointList.get(i);
-            System.out.println(points[i]);
+        for (int i=0; i < pointList.size(); ++i) { //convert ArrayList back
+            points[i] = pointList.get(i);          //to array
         }
         
         return points;
     }
     
-    private static ArrayList<Segment> segmentRemover(ArrayList<Segment> segmentList, int minEnd) {
-        
-        Iterator<Segment> it = segmentList.iterator();
+    private static ArrayList<Segment> segRemove(ArrayList<Segment> segList, int minEnd) {
+        //update segment list by removing all segments covered by current minEnd
+        Iterator<Segment> it = segList.iterator();
         
         while (it.hasNext()) {
             Segment seg = it.next();
@@ -37,16 +38,16 @@ public class CoveringSegments {
             }
         }
         
-        return segmentList;
+        return segList;
     }
 
-    private static int calcMinEnd(ArrayList<Segment> segmentList) {
-        
+    private static int calcMinEnd(ArrayList<Segment> segList) {
+        //returns the lowest end point of all segments in the current list
         int minEnd = 0;
         int currentValue;
         
-        for (int i=0; i < segmentList.size(); ++i) {
-            currentValue = (int) segmentList.get(i).end;
+        for (int i=0; i < segList.size(); ++i) {
+            currentValue = (int) segList.get(i).end;
             if (minEnd == 0) {
                 minEnd = currentValue;
             }
@@ -74,13 +75,12 @@ public class CoveringSegments {
             int start, end;
             start = scanner.nextInt();
             end = scanner.nextInt();
-            segments[i] = new Segment(start, end);
+            segments[i] = new Segment(start, end); //make a segment list out of cmd line args
         }
         int[] points = optimalPoints(segments);
-        //System.out.println(points.length);
+        System.out.println(points.length);
         for (int point : points) {
             System.out.print(point + " ");
         }
     }
 }
- 
